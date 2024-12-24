45640fe8dcd251fc5fa190c81b60914df876ce53
shafikcmt <mdshafiqulislam822@gmail.com>
2024-12-24 21:16:32 +0530

    Process staps section completed

--- FrontEnd/src/Pages/HomePage/HomePage.jsx
+++ FrontEnd/src/Pages/HomePage/HomePage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HeroSection from '../../Components/Navbar/HeroSection';
+import ProcessSteps from '../../Components/ProcessSteps/ProcessSteps';
 
 
 const HomePage = () => {
   return (
     <div>
      <HeroSection />
+     <ProcessSteps />
     </div>
   );
 };
